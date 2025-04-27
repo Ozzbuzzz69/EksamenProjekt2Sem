045fcdd7f76845d33dff38cee3e72e8b918a021a
MichaelTheDane <michael.schonemann@hotmail.dk>
2025-04-27 16:21:17 +0200

    Added domain services

---
+++ EksamenProjekt2Sem/Services/OrderService.cs
@@ -0,0 +1,10 @@
+﻿using EksamenProjekt2Sem.Models;
+
+namespace EksamenProjekt2Sem.Services
+{
+    public class OrderService
+    {
+        private List<Order> _orders;
+        private GenericService<Order> _dbService;
+    }
+}
---
+++ EksamenProjekt2Sem/Services/SandwichService.cs
@@ -0,0 +1,10 @@
+﻿using EksamenProjekt2Sem.Models;
+
+namespace EksamenProjekt2Sem.Services
+{
+    public class SandwichService
+    {
+        private List<Sandwich> _sandwiches;
+        private GenericService<Sandwich> _dbService;
+    }
+}
