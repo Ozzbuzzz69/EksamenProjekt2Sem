763ccff5b0a1bf12e9f0c550efa48b704816121f
MichaelTheDane <michael.schonemann@hotmail.dk>
2025-04-28 14:42:01 +0200

    Added Class Diagram's Services structure

--- EksamenProjekt2Sem/Services/OrderService.cs
+++ EksamenProjekt2Sem/Services/OrderService.cs
@@ -2,9 +2,49 @@
 
 namespace EksamenProjekt2Sem.Services
 {
-    public class OrderService
+    public class OrderService : GenericDbService<Order>
     {
-        private List<Order> _orders;
-        private GenericService<Order> _dbService;
+        private List<Order> _orders; // Overskud fra domain model
+        private GenericDbService<Order> _dbService; // Overskud fra domain model
+
+        public OrderService(OrderService orderService)
+        {
+            _orders = new List<Order>();
+            _dbService = orderService;
+        }
+        public OrderService()
+        {
+            _orders = new List<Order>();
+            _dbService = new GenericDbService<Order>();
+        }
+        public void CreateOrder(Order order)
+        {
+            // Add order to Database
+        }
+        public Order ReadOrder(int id)
+        {
+            // Read order from Database
+            return new Order(); // Placeholder return
+        }
+        public List<Order> ReadAllOrders()
+        {
+            // Read all orders from Database
+            return new List<Order>(); // Placeholder return
+        }
+        public void UpdateOrder(int id, Order order)
+        {
+            // Update order by id in Database
+        }
+        public Order DeleteOrder(int id)
+        {
+            // Delete order from Database
+            return new Order(); // Placeholder return
+        }
+        public double CalculateTotalPrice(List<OrderLine> orderlines)
+        {
+            // Go through each orderline in the list to calculate the total price
+
+            return 0; // Placeholder return
+        }
     }
 }
