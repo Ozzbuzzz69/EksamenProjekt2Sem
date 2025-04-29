3307ee672ae1b01c7123d1b8f5b3e40d581f8b3c
Oliver Ballegaard Westphalen <oliver_bal@hotmail.com>
2025-04-29 09:24:50 +0200

    Models

--- EksamenProjekt2Sem/Models/Order.cs
+++ EksamenProjekt2Sem/Models/Order.cs
@@ -1,11 +1,38 @@
-﻿namespace EksamenProjekt2Sem.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace EksamenProjekt2Sem.Models
 {
     public class Order
     {
+        
         public int Id { get; set; }
+
         public User User { get; set; }
+
+        
         public DateTime PickupTime { get; set; }
+
         public double TotalPrice { get; set; }
+
         private List<OrderLine> orderLines = new List<OrderLine>();
+
+        public Order()
+        { }
+
+        /// <summary>
+        /// Constructor for Order class
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="user"></param>
+        /// <param name="pickupTime"></param>
+        /// <param name="totalPrice"></param>
+        public Order(int id, User user, DateTime pickupTime, double totalPrice)
+        {
+            Id = id;
+            User = user;
+            PickupTime = pickupTime;
+            TotalPrice = totalPrice;
+        }
     }
 }
--- EksamenProjekt2Sem/Models/User.cs
+++ EksamenProjekt2Sem/Models/User.cs
@@ -1,12 +1,37 @@
-﻿namespace EksamenProjekt2Sem.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EksamenProjekt2Sem.Models
 {
     public class User
     {
+        
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string PhoneNumber { get; set; }
+
+        
         public string Password { get; set; }
+
+        public User()
+        { }
+
+        /// <summary>
+        /// Constructor for User class
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="email"></param>
+        /// <param name="phoneNumber"></param>
+        /// <param name="password"></param>
+        public User(int id, string name, string email, string phoneNumber, string password)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            PhoneNumber = phoneNumber;
+            Password = password;
+        }
 
     }
 }
