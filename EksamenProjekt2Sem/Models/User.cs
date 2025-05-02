e380ac4f0a3cb38d179ebb2ae127a23737ef74b3
Oscar Friis Ussing <osu001@edu.zealand.dk>
2025-05-02 10:56:54 +0200

    Author Oliver

--- EksamenProjekt2Sem/Models/User.cs
+++ EksamenProjekt2Sem/Models/User.cs
@@ -1,27 +1,42 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EksamenProjekt2Sem.Models
 {
     public class User
     {
-        
-        public int Id { get; private set; }
-        public string Name { get; private set; }
-        public string Email { get; private set; }
-        public string PhoneNumber { get; private set; }
-        public string Password { get; private set; }
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Navnet skal være udfyldt")]
+        [StringLength(50, ErrorMessage = "Navnet må max være 50 tegn langt")]
+        [RegularExpression(@"^[a-zA-ZæøåÆØÅ\s]+$", ErrorMessage = "Navnet må kun indeholde bogstaver og mellemrum")] // Regex til at sikre at der kun er bogstaver og mellemrum
+        [DataType(DataType.Text)]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Indtast gyldig Email")]
+        [EmailAddress(ErrorMessage = "Indtast gyldig email adresse")]
+        [StringLength(50, ErrorMessage = "Email må max være 50 tegn langt")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; } // Bruges som login
+
+        [Required(ErrorMessage = "Indtast gyldigt nummer")]
+        [RegularExpression(@"^+?[0-9]{8,10}$", ErrorMessage = "Indtast gyldigt telefonnummer")] // +45 12345678
+        [StringLength(10, ErrorMessage = "Telefonnummeret må max være 10 tegn langt")]
+        [DataType(DataType.PhoneNumber)]
+        public string PhoneNumber { get; set; }
+
+        [Required(ErrorMessage = "Indtast gyldig kode")]
+        [MinLength(8, ErrorMessage = "Koden skal minimum indeholde 8 tegn")]
+        [MaxLength(20, ErrorMessage = "Koden må max være 20 tegn langt")]
+        [RegularExpression(@"^(?=.[a-z])(?=.[A-Z])(?=.*\d)[a-zA-Z\d]{8,}$", ErrorMessage = "Koden skal indeholde mindst 1 stort bogstav, 1 lille bogstav og 1 tal")] // "\d" konverter tal til en string
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
 
         public User()
         { }
 
-        /// <summary>
-        /// Constructor for User class
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="name"></param>
-        /// <param name="email"></param>
-        /// <param name="phoneNumber"></param>
-        /// <param name="password"></param>
         public User(int id, string name, string email, string phoneNumber, string password)
         {
             Id = id;
@@ -29,64 +44,6 @@ namespace EksamenProjekt2Sem.Models
             Email = email;
             PhoneNumber = phoneNumber;
             Password = password;
-        }
-        /// <summary>
-        /// Function to set otherwise private id
-        /// </summary>
-        /// <param name="id"></param>
-        public void SetId(int id)
-        {
-            Id = id;
-        }
-        /// <summary>
-        /// Function to set otherwise private name
-        /// </summary>
-        /// <param name="name"></param>
-        public void SetName(string name)
-        {
-            Name = name;
-        }
-        /// <summary>
-        /// Function to set otherwise private email
-        /// </summary>
-        /// <param name="email"></param>
-        public void SetEmail(string email)
-        {
-            Email = email;
-        }
-        /// <summary>
-        /// Function to set otherwise private phone number
-        /// </summary>
-        /// <param name="phoneNumber"></param>
-        public void SetPhoneNumber(string phoneNumber)
-        {
-            PhoneNumber = phoneNumber;
-        }
-        /// <summary>
-        /// Function to set otherwise private password
-        /// </summary>
-        /// <param name="password"></param>
-        public void SetPassword(string password)
-        {
-            Password = password;
-        }
-        /// <summary>
-        /// Function to validate password
-        /// </summary>
-        /// <param name="password"></param>
-        /// <returns>bool</returns>
-        public bool ValidatePassword(string password)
-        {
-            return Password == password;
-        }
-        /// <summary>
-        /// Function to validate email
-        /// </summary>
-        /// <param name="email"></param>
-        /// <returns>bool</returns>
-        public bool ValidateEmail(string email)
-        {
-            return Email == email;
         }
 
     }
