f2be724734a26a3aaea085abf26bdaeef6894d9e
Oscar Friis Ussing <osu001@edu.zealand.dk>
2025-05-02 10:18:10 +0200

    Rettelse af orderLine

--- EksamenProjekt2Sem/Models/CampaignOffer.cs
+++ EksamenProjekt2Sem/Models/CampaignOffer.cs
@@ -1,59 +1,32 @@
-﻿namespace EksamenProjekt2Sem.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.RegularExpressions;
+
+namespace EksamenProjekt2Sem.Models
 {
     public class CampaignOffer
     {
-        public int Id { get; private set; }
-        public string Name { get; private set; }
-        public string Description { get; private set; }
-        public double Price { get; private set; }
-
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        
+        [Required]
+        [StringLength(100, ErrorMessage = "Maks 100 tegn")]
+        public string Name { get; set; }
+        [Required]
+        public string ImageLink { get; set; }
+        [Required]
+        public double Price { get; set; }
+        
         public CampaignOffer()
         { }
 
-        /// <summary>
-        /// Constructor for CampaignOffer class
-        /// </summary>
-        /// <param name="id"></param>
-        /// <param name="name"></param>
-        /// <param name="description"></param>
-        /// <param name="price"></param>
-        public CampaignOffer(int id, string name, string description, double price)
+       
+        public CampaignOffer(int id, string name, string imageLink, double price)
         {
             Id = id;
             Name = name;
-            Description = description;
-            Price = price;
-        }
-        /// <summary>
-        /// Function to set otherwise private id
-        /// </summary>
-        /// <param name="id"></param>
-        public void SetId(int id)
-        {
-            Id = id;
-        }
-        /// <summary>
-        /// Function to set otherwise private name
-        /// </summary>
-        /// <param name="name"></param>
-        public void SetName(string name)
-        {
-            Name = name;
-        }
-        /// <summary>
-        /// Function to set otherwise private description
-        /// </summary>
-        /// <param name="description"></param>
-        public void SetDescription(string description)
-        {
-            Description = description;
-        }
-        /// <summary>
-        /// Function to set otherwise private price
-        /// </summary>
-        /// <param name="price"></param>
-        public void SetPrice(double price)
-        {
+            ImageLink = imageLink;
             Price = price;
         }
     }
