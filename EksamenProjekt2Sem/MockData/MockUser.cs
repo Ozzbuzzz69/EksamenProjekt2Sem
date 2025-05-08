d1510cc8142a0a37c681197a38ffafa3025e8427
MichaelTheDane <michael.schonemann@hotmail.dk>
2025-05-08 11:31:27 +0200

    Added MockData

---
+++ EksamenProjekt2Sem/MockData/MockUser.cs
@@ -0,0 +1,18 @@
+﻿namespace EksamenProjekt2Sem.MockData
+{
+    using EksamenProjekt2Sem.Models;
+    public class MockUser
+    {
+        private static List<User> _users = new List<User>()
+        {
+            new("Carl", "test@example.com", "12345678", "1234"),
+            new("Lars", "lars@vontrier.dk", "69696969", "Nymphomaniac"),
+            new("Mike", "MikeHawk@TooBig.com", "80000003", "benis"),
+            new("Jarjar", "DarthJarJar@binks.ge", "19081999", "DarkSide")
+        };
+        public static List<User> GetUsers()
+        {
+            return _users;
+        }
+    }
+}
